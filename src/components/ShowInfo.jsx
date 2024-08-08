748de97c9c32e7af51dac3b095a9fbaaf2b8603c
kaufon <kauanfvale@gmail.com>
2024-08-08 20:20:16 -0300

    💄style: add style to show info

--- src/components/ShowInfo.jsx
+++ src/components/ShowInfo.jsx
@@ -1,13 +1,28 @@
-const ShowInfo = ({PersonalInfo }) => {
+import { Mail, Phone } from "lucide-react";
+
+const ShowInfo = ({ PersonalInfo }) => {
   return (
     <>
-      <div>
-        <h1>{PersonalInfo.name}</h1>
-        <h1>{PersonalInfo.surname}</h1>
-        <h1>{PersonalInfo.email}</h1>
-        <h1>{PersonalInfo.phone}</h1>
+      <div className="bg-blue-700 text-white flex justify-between h-32">
+        <div className="w-full flex justify-between m-6">
+          <div className="flex flex-col  justify-center items-center font-bold text-4xl h-full">
+            <h1>
+              {PersonalInfo.name} {PersonalInfo.surname}
+            </h1>
+          </div>
+          <div className="flex justify-center text-xl space-y-2 items-center flex-col ">
+            <span className="flex items-center gap-2 font-medium justify-center">
+              <Phone className="size-5 text-black" />
+              {PersonalInfo.phone}
+            </span>
+            <span className="flex items-center gap-2 font-medium justify-center">
+              <Mail className="size-5 text-black" />
+              {PersonalInfo.email}
+            </span>
+          </div>
+        </div>
       </div>
     </>
   );
-}
+};
 export default ShowInfo;

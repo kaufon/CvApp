d28955b380e1bf7366ef0751e2d1bf39656ba6ad
kaufon <kauanfvale@gmail.com>
2024-08-09 17:00:35 -0300

    ♻️ refactor: new able to see degrees

--- src/components/ShowInfo.jsx
+++ src/components/ShowInfo.jsx
@@ -1,6 +1,6 @@
 import { Mail, Phone } from "lucide-react";
 
-const ShowInfo = ({ PersonalInfo , Degrees}) => {
+const ShowInfo = ({ PersonalInfo, Degrees }) => {
   return (
     <>
       <div className="bg-blue-700 text-white flex justify-between h-32">
@@ -21,6 +21,23 @@ const ShowInfo = ({ PersonalInfo , Degrees}) => {
             </span>
           </div>
         </div>
+      </div>
+
+      <div>
+        {Degrees.map((degree) => (
+          <div
+            key={degree.id}
+            className="flex flex-col font-medium text-xl m-4 space-y-5"
+          >
+            <div className="flex flex-col space-y-2 flex-1 ">
+              <span className="font-bold italic">{degree.degreeName}</span>
+
+              <span className="flex flex-1 w-full text-gray-600 font-bold">{degree.degree} | {degree.year} </span>
+            </div>
+
+            <div className="h-px bg-black w-full" />
+          </div>
+        ))}
       </div>
     </>
   );
